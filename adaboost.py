da0930fcc7cd7569a128ff673d016d86fa033a9f
Gary Doran, Jr <garydoranjr@gmail.com>
2011-11-13 23:32:24 -0500

    Implement Adaboost

--- adaboost.py
+++ adaboost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from math import log, pi, exp
+from itertools import count
 
 DATA =   [(0, 1), (0, 0), (1, 0), (2, 2)]
 LABELS = [ True,  False,   True,  False ]
@@ -76,12 +77,105 @@ class ClassModel(object):
     def log_likelihood(self, label):
         return log(self.probs[label])
 
-def adaboost(data, labels, step=True):
-    pass
+class AdaboostClassifier(object):
+    """
+    Combines individual weighted classifiers
+    """
+    def __init__(self, classifiers, alphas):
+        self.classifiers = classifiers
+        Z = sum(alphas)
+        self.alphas = [a / Z for a in alphas]
+
+    def classify(self, instance, verbose=True):
+        lines = list()
+        avg = 0
+        first = True
+        for h, a in zip(self.classifiers, self.alphas):
+            if first:
+                indent = 'f(x) = '
+                first = False
+            else:
+                indent = '       '
+            cls = 2*h.classify(instance) - 1
+            avg += a*cls
+            lines.append(indent + ('%.5f * (%2d)' % (a, cls)))
+        print ' +\n'.join(lines)
+        print avg
+        return (avg >= 0)
+
+def adaboost(data, labels, verbose=True, step=True):
+    #Initialize weights
+    weights = [1.0 / len(data) for i in range(len(data))]
+    classifiers = list()
+    alphas = list()
+    for iteration in count(1):
+        print 79*'*'
+        print 'Iteration %d...' % iteration
+        print 79*'*'
+        print
+        print 'Weighted Examples:'
+        print_dataset(data, weights, labels)
+        print
+
+        print 'Training Classifier...'
+        classifier = NaiveBayesClassifier(data, weights, labels)
+        print
+
+        print 'Predictions:'
+        predictions = map(classifier.classify, data)
+        print_dataset(data, weights, labels, predictions)
+        print
+
+        epsilon = sum(w for w, l, p in zip(weights, labels, predictions)
+                        if l != p)
+        print 'epsilon_%d: %.5f' % (iteration, epsilon)
+        if epsilon == 0:
+            print 'Breaking since epsilon_%d = 0' % iteration
+            break
+
+        alpha = 0.5*log((1 - epsilon) / epsilon)
+        print 'alpha_%d: %.5f' % (iteration, alpha)
+        print
+
+        classifiers.append(classifier)
+        alphas.append(alpha)
+
+        if epsilon > 0.5:
+            print 'Breaking since epsilon_%d > 0.5' % iteration
+            break
+
+        print 'Updating Weights...'
+        factors = [(2*l - 1)*(2*p - 1) for l, p in zip(labels, predictions)]
+        weights = [w*exp(-alpha*f) for w, f in zip(weights, factors)]
+        Z = sum(weights)
+        weights = [w/Z for w in weights]
+
+    return AdaboostClassifier(classifiers, weights)
+
+def print_dataset(data, weights, labels, predictions=None):
+    assert len(data[0]) == 2
+    if predictions is None:
+        print '+-------+--------+-------+'
+        print '|  w_i  |  data  | label |'
+        print '+-------+--------+-------+'
+        for row, w, l in zip(data, weights, labels):
+            datastr = '  '.join(map(str, row))
+            print  '| %.3f |  %s  |%s|' % (w, datastr, str(l).center(7))
+        print '+-------+--------+-------+'
+    else:
+        print '+-------+--------+-------+------------+'
+        print '|  w_i  |  data  | label | prediction |'
+        print '+-------+--------+-------+------------+'
+        for row, w, l, p in zip(data, weights, labels, predictions):
+            datastr = '  '.join(map(str, row))
+            line = ('| %.3f |  %s  |%s|%s|' %
+                    (w, datastr, str(l).center(7), str(p).center(12)))
+            print line
+        print '+-------+--------+-------+------------+'
 
 def main(step):
-    nb = NaiveBayesClassifier(DATA, [0.25, 0.25, 0.25, 0.25], LABELS)
-    print nb.confidence((1,1))
+    ab = adaboost(DATA, LABELS)
+    print ab.classify((2, 0))
 
 if __name__ == '__main__':
     from sys import argv
